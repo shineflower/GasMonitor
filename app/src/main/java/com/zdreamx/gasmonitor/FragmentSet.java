f6fea0e80f2f81bfdc020327dadd4043911b7102
Ashia Huang <ashia.huang@gmail.com>
2015-07-06 22:34:48 +0800

    更新设置

--- app/src/main/java/com/zdreamx/gasmonitor/FragmentSet.java
+++ app/src/main/java/com/zdreamx/gasmonitor/FragmentSet.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,25 +16,26 @@ import android.view.ViewGroup;
 /**
  * Created by zdreamx on 2015/3/26.
  */
-public class FragmentSet extends Fragment {
-    @Nullable
+public class FragmentSet extends PreferenceFragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_set,container,false);
-        //取消登录
-        view.findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences sp = getActivity().getSharedPreferences("setting", Context.MODE_PRIVATE );
-                SharedPreferences.Editor editor = sp.edit();
-                editor.remove("uname");
-                editor.remove("passwd");
-                editor.commit();
-                Intent intent = new Intent(getActivity(),LoginActivity.class);
-                startActivity(intent);
-                getActivity().finish();
-            }
-        });
-        return view;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings_preference);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        String key = preference.getKey();
+        if ("logout".equals(key)) {
+            SharedPreferences sp = getActivity().getSharedPreferences("setting", Context.MODE_PRIVATE );
+            SharedPreferences.Editor editor = sp.edit();
+            editor.remove("uname");
+            editor.remove("passwd");
+            editor.commit();
+            Intent intent = new Intent(getActivity(),LoginActivity.class);
+            startActivity(intent);
+            getActivity().finish();
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 }
